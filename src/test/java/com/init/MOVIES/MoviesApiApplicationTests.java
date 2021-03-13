814da7f7bc4784a89ca98aed04af59ecbe094fc8
Sebas <seplarui@gmail.com>
2021-03-14 00:26:36 +0100

    first commit

---
+++ src/test/java/com/init/MOVIES/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.MOVIES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
